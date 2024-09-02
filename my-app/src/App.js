71e76b87156a64c01f2215756f524a1694175b58
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-09-02 21:32:05 +0300

    feat: add form post

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,31 @@
 import React, {useState} from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
-    const [posts, setPost] = useState([
+    const [posts, setPosts] = useState([
         {id: 1, title: 'JavaScript 1', body: 'Description'},
         {id: 2, title: 'JavaScript 2', body: 'Description'},
         {id: 3, title: 'JavaScript 3', body: 'Description'}
     ]);
 
+    function createPost(newPost) {
+        setPosts( [...posts, newPost]);
+    }
+
+    // получаем пост из дочернего
+    function removePost(post) {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
     return (
         <div className="App">
-            <PostList posts={posts} title= "Посты JS"/>
+            <PostForm create={createPost}/>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title= "Посты JS"/>
+                : <h1>Посты не найдены!</h1>
+            }
         </div>
     );
 }
